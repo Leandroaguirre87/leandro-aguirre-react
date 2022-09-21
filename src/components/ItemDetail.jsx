5c2bddfc8bcd8b3af1cd6b96fa9eb10f9f123ccb
Leandroaguirre87 <103083101+Leandroaguirre87@users.noreply.github.com>
2022-09-20 22:20:17 -0300

    se agrego goToCart

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import ItemCount from "./ItemCount";
 
 const ItemDetail = ({ data }) => {
-  const onAdd = () => {
-    console.log("agregaste al carrito");
+  const [goToCart, setGoToCart] = useState(false);
+
+  const onAdd = (quantity) => {
+    console.log(`compraste ${quantity} unidades`);
+    setGoToCart(true);
   };
 
   return (
@@ -11,8 +15,12 @@ const ItemDetail = ({ data }) => {
       <img src={data.image} width={100} alt={data.title} />
       <div>
         <h2 className="text-center">{data.title}</h2>
-        <p className="text-center">${data.price}</p>
-        <ItemCount initial={1} stock={5} onAdd={onAdd} />
+        <h1 className="text-center">${data.price}</h1>
+        {goToCart ? (
+          <Link to="/cart">Finalizar Compra</Link>
+        ) : (
+          <ItemCount initial={1} stock={5} onAdd={onAdd} />
+        )}
       </div>
     </div>
   );
